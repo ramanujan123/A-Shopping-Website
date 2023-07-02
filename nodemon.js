bdda8cd1d77d69523a9a70e87e5adc3fedc6f422
Mitul <mitulvardhan@iitbhilai.ac.in>
2023-07-03 03:25:08 +0530

    final commit

---
+++ nodemon.js
@@ -0,0 +1,8 @@
+// {
+//     "env":{
+//         "MONGO_USER ": "mitulvardhan",
+//         "MONGO_PASSWORD": "Mitul123",
+//         "MONGO_DEFAULT_DATABASE": "shop",
+//         "STRIPE_KEY": "sk_test_51NMRiRSAtlptADKTGmEMJ97OVQVxmGbcl7angcd7nR7KvM9zPdirBKJKMPwknz7ecrYr3RUPzvJNOfc50qoRm67300k9UyGfXW"
+//     }
+// }
